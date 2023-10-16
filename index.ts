98a862404b4b7fdc6fe50ceca466cc32849ee39b
John Carmack <johncarmack@me.com>
2023-10-15 17:06:58 -0700

    Add request logging and health check endpoint

--- index.ts
+++ index.ts
@@ -1,7 +1,14 @@
 const server = Bun.serve({
   hostname: "::",
   port: process.env.PORT ?? 3000,
-  fetch(request) {
+  fetch(request: Request) {
+    const log = {
+      referrer: request.referrer || "noreferrer",
+      method: request.method,
+      url: new URL(request.url).pathname,
+    };
+    console.info([...Object.values(log), "200"]);
+    if (log.url === "/health") return new Response("OK");
     return new Response("Hello from Bun!");
   },
 });
